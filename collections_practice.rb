a014fe040c580d6f69cb1542cec59f9e37408c65
Seth Todd <seth.todd@outlook.com>
2020-02-05 03:23:19 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -17,6 +17,8 @@ def swap_elements(array)
   array
 end
 
+
+
 def swap_elements_from_to(array, index, destination_index)
   array[index], array[destination_index] = array[destination_index], array[index]
   array
