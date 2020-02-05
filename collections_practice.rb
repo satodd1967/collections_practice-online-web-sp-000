16ebfaf5fde7d5e411de3dd808c73ff10e13b727
Seth Todd <seth.todd@outlook.com>
2020-02-05 03:24:35 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,70 @@
+def sort_array_asc(array)
+  array.sort
+end
+
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+stuff = ["a", "b", "c"]
+
+def swap_elements_from_to(array, index, destination_index)
+  array[index], array[destination_index] = array[destination_index], array[index]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+m = ["blake", "ashley", "scott"]
+
+def kesha_maker(array)
+  cash = []
+  array.each do |money|
+    cash << money[0..1] + money[2].replace("$") + money[3..]
+  end
+  cash
+end
+
+def find_a(array)
+  array.select do |a|
+    a.start_with?("a")
+  end
+end
+
+
+def sum_array(array)
+  array.inject(0) do |memo, item|
+    memo + item
+  end
+end
+
+stuff = ["hand","feet", "knee", "table"]
+
+def add_s(array)
+  array.each_with_index.collect do |word, index|
+    if index == 1
+      word
+    else
+      word + "s"
+    end
+end
+end
+
+
+
+
+
 
